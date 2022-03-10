12bff0a42f29ea03d97f236c11f9e47d06604a2c
Alief <aliefiantoarths@gmail.com>
2022-03-10 21:12:48 +0800

    Add new duplicate file

---
+++ python_login_failed _duplicate.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Firefox()
+driver.get("https://www.saucedemo.com") #before pre
+assert "Swag Labs" in driver.title #ngecheck udah di python org
+elem = driver.find_element_by_id("user-name") #object = locator #find_element = selector
+elem.send_keys("standard_user")
+elem = driver.find_element_by_id("password") 
+elem.send_keys("falsepassword")
+elem.send_keys(Keys.RETURN)
+assert "Epic sadface:" in driver.find_element_by_xpath("//h3[@data-test='error']").text
+
+#Third Commit
